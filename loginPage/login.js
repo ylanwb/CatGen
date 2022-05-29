76e5779cc916074a357a1100d80aa20871a4a43b
ylanwb <class-computer@class-computers-MacBook-Air.local>
2022-05-29 14:18:08 -0700

    finished js html

---
+++ loginPage/login.js
@@ -0,0 +1,97 @@
+console.log("test")
+const mainContainer = document.getElementById("mainContainer")
+const loginBox = document.createElement("div")
+const loginContent = document.createElement("div")
+const h1 = document.createElement("h1")
+const welcomeMsg = document.createElement("p")
+const sleepyCat = document.createElement("img")
+const userLogin = document.createElement("div")
+
+const inputUserName = document.createElement("input")
+inputUserName.classList.add("userInputs");
+inputUserName.setAttribute("id", "username");
+inputUserName.type = "text";
+inputUserName.required = "true";
+const labelUserName = document.createElement("label")
+labelUserName.setAttribute("id", "labelId");
+labelUserName.setAttribute("for", "username");
+const userName = document.createElement("span")
+userName.setAttribute("id", "infoOne");
+userName.innerText = "Username";
+
+const inputPassword = document.createElement("input")
+inputPassword.classList.add("userInputs");
+inputPassword.setAttribute("id", "password");
+inputPassword.type = "text";
+inputPassword.required = "true";
+const labelPassword = document.createElement("label")
+labelPassword.setAttribute("id", "labelId");
+labelPassword.setAttribute("for", "password");
+const password = document.createElement("span")
+password.setAttribute("id", "infoTwo");
+password.innerText = "Password";
+
+const loginError = document.createElement("span")
+loginError.classList.add("requiredFields");
+loginError.classList.add("hide");
+loginError.setAttribute("id", "loginError");
+loginError.innerText = "Invalid Username or Password"
+
+const href = document.createElement("a");
+href.setAttribute("href", "#");
+const button = document.createElement("button");
+button.setAttribute("id", "button");
+button.innerText = "Sign in!";
+
+const createAccountContainer = document.createElement("div")
+createAccountContainer.classList.add("createAccount")
+const newHere = document.createElement("span")
+newHere.innerText = "New Here?"
+const createAccount = document.createElement("a")
+createAccount.setAttribute("href", "../registerPage/register.html")
+createAccount.innerText = " Create an Account"
+
+const catPicBox = document.createElement("div")
+catPicBox.classList.add("catPicBox")
+const catOne = document.createElement("img")
+catOne.setAttribute("src", "https://storables.com/wp-content/uploads/2020/07/18-inch-Cat-Shelf-1024x521.jpg")
+catOne.setAttribute("id", "catOne")
+const catTwo = document.createElement("img")
+catTwo.setAttribute("src", "https://media.istockphoto.com/photos/ginger-cat-picture-id1073474110?k=20&m=1073474110&s=612x612&w=0&h=Yxf5oJ9FK7Al3VsMmsQUEm3eRgzeYdenBWfk3zxip1c=")
+catTwo.setAttribute("id", "catTwo")
+
+loginBox.classList.add("loginBox")
+loginContent.classList.add("loginContent")
+h1.innerText = "Cat Image Generator"
+welcomeMsg.innerText = "Welcome Back!"
+sleepyCat.setAttribute("id", "sleepyCat")
+sleepyCat.setAttribute("src", "https://media.istockphoto.com/photos/sleepy-kitten-picture-id146869137?k=20&m=146869137&s=612x612&w=0&h=6A32QaIlhVX1YxGEtcapbc2mI82gCsNmh4D3OhKuz6Y=")
+userLogin.classList.add("userLogin")
+
+mainContainer.appendChild(loginBox)
+loginBox.appendChild(loginContent)
+loginContent.appendChild(h1)
+loginContent.appendChild(welcomeMsg)
+loginContent.appendChild(sleepyCat)
+loginContent.appendChild(userLogin)
+
+userLogin.appendChild(inputUserName)
+userLogin.appendChild(labelUserName)
+labelUserName.appendChild(userName)
+
+userLogin.appendChild(inputPassword)
+userLogin.appendChild(labelPassword)
+labelPassword.appendChild(password)
+
+userLogin.appendChild(loginError)
+
+userLogin.appendChild(href)
+href.appendChild(button)
+
+loginContent.appendChild(createAccountContainer)
+createAccountContainer.appendChild(newHere)
+createAccountContainer.appendChild(createAccount)
+
+mainContainer.appendChild(catPicBox)
+catPicBox.appendChild(catOne)
+catPicBox.appendChild(catTwo)
--- registerPage/register.js
+++ registerPage/register.js
@@ -1,144 +1,214 @@
 const mainContainer = document.getElementById("main-container");
-const signUpBox = document.createElement("div")
-const signUpContent = document.createElement("div")
+const signUpContainer = document.createElement("div");
+
 const catPic = document.createElement("div");
-const h1 = document.createElement("h1")
-const circleCat = document.createElement("img")
-const userSignUp = document.createElement("div")
+const h1 = document.createElement("h1");
+const circleCat = document.createElement("img");
+const userSignUp = document.createElement("div");
 
-const inputUserName = document.createElement("input")
-inputUserName.classList.add("userInputs")
-inputUserName.setAttribute("id", "username")
-inputUserName.type = "text"
-inputUserName.required = "true"
-const labelUsername = document.createElement("label")
-labelUsername.setAttribute("id", "labelId")
-labelUsername.setAttribute("for", "username")
-const userName = document.createElement("span")
-userName.setAttribute("id", "infoOne")
-userName.innerText = "Username"
-const userNameError = document.createElement("span")
-userNameError.classList.add("requiredFields")
-userNameError.classList.add("hide")
-userNameError.setAttribute("id", "userNameError")
-const userNameErrorTwo = document.createElement("span")
-userNameErrorTwo.classList.add("requiredAmount")
-userNameErrorTwo.classList.add("hide")
-userNameErrorTwo.setAttribute("id", "userNameErrorTwo")
+const inputUserName = document.createElement("input");
+inputUserName.classList.add("userInputs");
+inputUserName.setAttribute("id", "username");
+inputUserName.type = "text";
+inputUserName.required = "true";
+const labelUsername = document.createElement("label");
+labelUsername.setAttribute("id", "labelId");
+labelUsername.setAttribute("for", "username");
+const userName = document.createElement("span");
+userName.setAttribute("id", "infoOne");
+userName.innerText = "Username";
+const userNameError = document.createElement("span");
+userNameError.classList.add("requiredFields");
+userNameError.classList.add("hide");
+userNameError.setAttribute("id", "userNameError");
+const userNameErrorTwo = document.createElement("span");
+userNameErrorTwo.classList.add("requiredAmount");
+userNameErrorTwo.classList.add("hide");
+userNameErrorTwo.setAttribute("id", "userNameErrorTwo");
 
-h1.innerText = "Cat Picture Generator"
-circleCat.src = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=640:*"
-circleCat.setAttribute("id", "circleCat")
+const inputEmail = document.createElement("input");
+inputEmail.classList.add("userInputs");
+inputEmail.setAttribute("id", "email");
+inputEmail.type = "text";
+inputEmail.required = "true";
+const labelEmail = document.createElement("label");
+labelEmail.setAttribute("id", "labelId");
+labelEmail.setAttribute("for", "email");
+const email = document.createElement("span");
+email.setAttribute("id", "infoTwo");
+email.innerText = "Email Address";
+const emailError = document.createElement("span");
+emailError.classList.add("requiredFields");
+emailError.classList.add("hide");
+emailError.setAttribute("id", "emailError");
+const emailErrorTwo = document.createElement("span");
+emailErrorTwo.classList.add("requiredAmount");
+emailErrorTwo.classList.add("hide");
+emailErrorTwo.setAttribute("id", "emailErrorTwo");
 
-signUpBox.classList.add("signUpBox")
-signUpContent.classList.add("signUpContent")
+const inputPassword = document.createElement("input");
+inputPassword.classList.add("userInputs");
+inputPassword.setAttribute("id", "password");
+inputPassword.type = "text";
+inputPassword.required = "true";
+const labelPassword = document.createElement("label");
+labelPassword.setAttribute("id", "labelId");
+labelPassword.setAttribute("for", "password");
+const password = document.createElement("span");
+password.setAttribute("id", "infoThree");
+password.innerText = "Password";
+const passwordError = document.createElement("span");
+passwordError.classList.add("requiredFields");
+passwordError.classList.add("hide");
+passwordError.setAttribute("id", "passwordError");
+const passwordErrorTwo = document.createElement("span");
+passwordErrorTwo.classList.add("requiredAmount");
+passwordErrorTwo.classList.add("hide");
+passwordErrorTwo.setAttribute("id", "passwordErrorTwo");
+
+const buttonContainer = document.createElement("div");
+buttonContainer.setAttribute("id", "buttonContainer");
+const href = document.createElement("a");
+href.setAttribute("href", "#");
+const button = document.createElement("button");
+button.setAttribute("id", "button");
+button.innerText = "Sign Up!";
+const createAccount = document.createElement("span");
+createAccount.innerText = "Have an Account?";
+const signInHref = document.createElement("a");
+signInHref.setAttribute("href", "../loginPage/login.html");
+signInHref.textContent = " Sign in!";
+
+const br = document.createElement("br");
+
+userNameError.innerText = "Please enter your Username";
+userNameErrorTwo.innerText = "Username needs to be more than three characters";
+
+emailError.innerText = "Please enter your Email Address";
+emailErrorTwo.innerText = "Email Adress is invalid";
+
+passwordError.innerText = "Please enter your Password";
+passwordErrorTwo.innerText = "Password needs to be more than seven characters";
+
+h1.innerText = "Cat Picture Generator";
+circleCat.src =
+  "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-photos-1593441022.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=640:*";
+circleCat.setAttribute("id", "circleCat");
+
+signUpContainer.classList.add("signUpContainer");
+
 catPic.classList.add("catPic");
 mainContainer.insertBefore(catPic, mainContainer.firstChild);
-mainContainer.appendChild(signUpBox)
-signUpBox.appendChild(signUpContent)
-signUpContent.appendChild(h1)
-signUpContent.appendChild(circleCat)
-signUpContent.appendChild(userSignUp)
-userSignUp.classList.add("userSignUp")
-userSignUp.appendChild(inputUserName)
-userSignUp.appendChild(labelUsername)
-labelUsername.appendChild(userName)
+mainContainer.appendChild(signUpContainer);
+signUpContainer.appendChild(h1);
+signUpContainer.appendChild(circleCat);
+signUpContainer.appendChild(userSignUp);
+userSignUp.classList.add("userSignUp");
 
-// const userName = document.getElementById("username");
-// const email = document.getElementById("email");
-// const password = document.getElementById("password");
+userSignUp.appendChild(inputUserName);
+userSignUp.appendChild(labelUsername);
+labelUsername.appendChild(userName);
+userSignUp.appendChild(userNameError);
+userSignUp.appendChild(userNameErrorTwo);
 
-// const userNameError = document.getElementById("userNameError");
-// const userNameErrorTwo = document.getElementById("userNameErrorTwo");
-// const emailError = document.getElementById("emailError");
-// const emailErrorTwo = document.getElementById("emailErrorTwo");
-// const passwordError = document.getElementById("passwordError");
-// const passwordErrorTwo = document.getElementById("passwordErrorTwo");
+userSignUp.appendChild(inputEmail);
+userSignUp.appendChild(labelEmail);
+labelEmail.appendChild(email);
+userSignUp.appendChild(emailError);
+userSignUp.appendChild(emailErrorTwo);
 
-// const button = document.getElementById("button");
+userSignUp.appendChild(inputPassword);
+userSignUp.appendChild(labelPassword);
+labelPassword.appendChild(password);
+userSignUp.appendChild(passwordError);
+userSignUp.appendChild(passwordErrorTwo);
 
-// const hover = document.querySelector(":hover");
+signUpContainer.appendChild(buttonContainer);
+buttonContainer.appendChild(href);
+buttonContainer.appendChild(br);
+href.appendChild(button);
+buttonContainer.appendChild(createAccount);
+createAccount.appendChild(signInHref);
 
-// const href = document.querySelector("a");
-// let userNameErrors = true;
-// let emailErrors = true;
-// let passwordErrors = true;
+let userNameErrors = true;
+let emailErrors = true;
+let passwordErrors = true;
 
-// function checkFirstName(value) {
-//   if (value === "") {
-//     userNameError.style.display = "flex";
-//     userNameErrorTwo.style.display = "none";
-//     button.setAttribute("disabled", "true");
-//     hover;
-//   } else if (value.length <= 3) {
-//     userNameErrorTwo.style.display = "flex";
-//     userNameError.style.display = "none";
-//     button.setAttribute("disabled", "true");
-//     console.log("Insufficient Character Amount");
-//   } else {
-//     userNameErrorTwo.style.display = "none";
-//     userNameError.style.display = "none";
-//     button.removeAttribute("disabled");
-//     userNameErrors = false;
-//     console.log(value);
-//   }
-// }
-// function checkEmail(value) {
-//   if (value === "") {
-//     emailError.style.display = "flex";
-//     emailErrorTwo.style.display = "none";
-//     button.setAttribute("disabled", "true");
-//   } else if (value.length <= 3) {
-//     emailErrorTwo.style.display = "flex";
-//     emailError.style.display = "none";
-//     console.log("Insufficient Character Amount");
-//     button.setAttribute("disabled", "true");
-//   } else {
-//     emailErrorTwo.style.display = "none";
-//     emailError.style.display = "none";
-//     console.log(value);
-//     button.removeAttribute("disabled");
-//     emailErrors = false;
-//   }
-// }
-// function checkPassword(valueOfPassword) {
-//   if (valueOfPassword === "") {
-//     passwordError.style.display = "flex";
-//     passwordErrorTwo.style.display = "none";
-//     button.setAttribute("disabled", "true");
-//   } else if (valueOfPassword.length <= 7) {
-//     passwordErrorTwo.style.display = "flex";
-//     passwordError.style.display = "none";
-//     console.log("Insufficient Character Amount");
-//     button.setAttribute("disabled", "true");
-//   } else {
-//     passwordError.style.display = "none";
-//     passwordErrorTwo.style.display = "none";
-//     console.log(valueOfPassword);
-//     button.removeAttribute("disabled");
-//     passwordErrors = false;
-//   }
-// }
+function checkFirstName(value) {
+  console.log(value);
+  if (value === "") {
+    userNameError.style.display = "flex";
+    userNameErrorTwo.style.display = "none";
+    button.setAttribute("disabled", "true");
+  } else if (value.length <= 3) {
+      userNameErrorTwo.style.display = "flex";
+      userNameError.style.display = "none";
+      button.setAttribute("disabled", "true");
+      console.log("Insufficient Character Amount");
+    } else {
+      userNameErrorTwo.style.display = "none";
+      userNameError.style.display = "none";
+      button.removeAttribute("disabled");
+      userNameErrors = false;
+      console.log(value);
+    }
+}
+function checkEmail(value) {
+  if (value === "") {
+    emailError.style.display = "flex";
+    emailErrorTwo.style.display = "none";
+    button.setAttribute("disabled", "true");
+  } else if (value.length <= 3) {
+    emailErrorTwo.style.display = "flex";
+    emailError.style.display = "none";
+    console.log("Insufficient Character Amount");
+    button.setAttribute("disabled", "true");
+  } else {
+    emailErrorTwo.style.display = "none";
+    emailError.style.display = "none";
+    console.log(value);
+    button.removeAttribute("disabled");
+    emailErrors = false;
+  }
+}
+function checkPassword(valueOfPassword) {
+  if (valueOfPassword === "") {
+    passwordError.style.display = "flex";
+    passwordErrorTwo.style.display = "none";
+    button.setAttribute("disabled", "true");
+  } else if (valueOfPassword.length <= 7) {
+    passwordErrorTwo.style.display = "flex";
+    passwordError.style.display = "none";
+    console.log("Insufficient Character Amount");
+    button.setAttribute("disabled", "true");
+  } else {
+    passwordError.style.display = "none";
+    passwordErrorTwo.style.display = "none";
+    console.log(valueOfPassword);
+    button.removeAttribute("disabled");
+    passwordErrors = false;
+  }
+}
 
-// userName.onkeyup = function () {
-//   checkFirstName(userName.value);
-// };
-// email.onkeyup = function () {
-//   checkEmail(email.value);
-// };
-// password.onkeyup = function () {
-//   checkPassword(password.value);
-// };
+inputUserName.onkeyup = function () {
+  checkFirstName(inputUserName.value);
+};
+inputEmail.onkeyup = function () {
+  checkEmail(inputEmail.value);
+};
+inputPassword.onkeyup = function () {
+  checkPassword(inputPassword.value);
+};
 
-// button.addEventListener("click", function () {
-//   checkFirstName(userName.value);
-//   checkEmail(email.value);
-//   checkPassword(password.value);
-//   console.log("clickedButton");
-//   if (!userNameErrors && !emailErrors && !passwordErrors === true) {
-//     href.setAttribute("href", "http://127.0.0.1:5500/loginPage/login.html#");
-//     console.log(button);
-//   } else {
-//     console.log("Error");
-//   }
-// });
+button.addEventListener("click", function () {
+  checkFirstName(inputUserName.value);
+    checkEmail(inputEmail.value);
+    checkPassword(inputPassword.value);
+  console.log("clickedButton");
+  if (!userNameErrors && !emailErrors && !passwordErrors === true) {
+    href.setAttribute("href", "http://127.0.0.1:5500/loginPage/login.html#");
+    console.log(button);
+  } else {
+    console.log("Error");
+  }
+});
