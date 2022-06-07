415ee15384a524059a62a3f2ef1e720db4f6d7d9
ylanwb <class-computer@class-computers-MacBook-Air.local>
2022-06-06 19:35:00 -0700

    progress on home

--- src/homePage/home.js
+++ src/homePage/home.js
@@ -1,20 +1,50 @@
 console.log("it is working");
 const randomCatPic = document.getElementById("randomCatPic");
 const generateButton = document.getElementById("catGeneratorBtn");
-const api_key = "https://aws.random.cat/meow";
+const api_key = "ad008c5d-682d-4ae4-a30e-3455d75b4ff9";
+const href = document.getElementById("signInHref");
+// const signInImg = document.getElementById("signInImg")
 
+async function getApi(apiKey) {
+  const response = await fetch("https://api.thecatapi.com/v1/images/search", {
+    mode: "cors",
+    method: "GET",
+    headers: {
+      "X-API-KEY": apiKey,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
 
-async function getApi(url) {
-  const response = await fetch(url);
-
-  const data = await response.json();
-  const imageSource = data?.file;
-
-  randomCatPic.setAttribute("src", imageSource);
+  const url = response[0].url;
+  randomCatPic.setAttribute("src", url);
 }
 
 window.onload = () => getApi(api_key);
 
 generateButton.addEventListener("click", () => {
-    getApi(api_key);
+  getApi(api_key);
+});
+const userLoggedIn = localStorage.getItem("user");
+
+const signInButton = document.getElementById("signInBtn");
+if (userLoggedIn === "true") {
+  console.log("loggedin");
+  // signInImg.setAttribute("src", "https://cdn-icons-png.flaticon.com/512/126/126467.png")
+  const newImg = document.createElement("img")
+  newImg.setAttribute('src', "https://cdn-icons-png.flaticon.com/512/126/126467.png")
+  newImg.setAttribute("id", "signInImg")
+  const newText = document.createElement("span")
+  newText.innerText = "Log-Out"
+  signInButton.appendChild(newImg)
+  href.href = "home.html"
+} else {
+  console.log("logout");
+  // signInButton.textContent = "Login";
+}
+
+signInButton.addEventListener("click", () => {
+  localStorage.removeItem("user");
 });
--- src/login.js
+++ src/login.js
@@ -1,41 +1,42 @@
-console.log("test")
-const mainContainer = document.getElementById("mainContainer")
-const loginBox = document.createElement("div")
-const loginContent = document.createElement("div")
-const h1 = document.createElement("h1")
-const welcomeMsg = document.createElement("p")
-const sleepyCat = document.createElement("img")
-const userLogin = document.createElement("div")
+console.log("test");
 
-const inputEmailAddress = document.createElement("input")
+const mainContainer = document.getElementById("mainContainer");
+const loginBox = document.createElement("div");
+const loginContent = document.createElement("div");
+const h1 = document.createElement("h1");
+const welcomeMsg = document.createElement("p");
+const sleepyCat = document.createElement("img");
+const userLogin = document.createElement("div");
+
+const inputEmailAddress = document.createElement("input");
 inputEmailAddress.classList.add("userInputs");
 inputEmailAddress.setAttribute("id", "email");
 inputEmailAddress.type = "text";
 inputEmailAddress.required = "true";
-const labelEmailAddress = document.createElement("label")
+const labelEmailAddress = document.createElement("label");
 labelEmailAddress.setAttribute("id", "labelId");
 labelEmailAddress.setAttribute("for", "email");
-const emailAddress = document.createElement("span")
+const emailAddress = document.createElement("span");
 emailAddress.setAttribute("id", "infoOne");
 emailAddress.innerText = "Email Address";
 
-const inputPassword = document.createElement("input")
+const inputPassword = document.createElement("input");
 inputPassword.classList.add("userInputs");
 inputPassword.setAttribute("id", "password");
 inputPassword.type = "password";
 inputPassword.required = "true";
-const labelPassword = document.createElement("label")
+const labelPassword = document.createElement("label");
 labelPassword.setAttribute("id", "labelId");
 labelPassword.setAttribute("for", "password");
-const password = document.createElement("span")
+const password = document.createElement("span");
 password.setAttribute("id", "infoTwo");
 password.innerText = "Password";
 
-const loginError = document.createElement("span")
+const loginError = document.createElement("span");
 loginError.classList.add("requiredFields");
 loginError.classList.add("hide");
 loginError.setAttribute("id", "loginError");
-loginError.innerText = "Invalid Username or Password"
+loginError.innerText = "Invalid Username or Password";
 
 const href = document.createElement("a");
 href.setAttribute("href", "#");
@@ -43,55 +44,64 @@ const button = document.createElement("button");
 button.setAttribute("id", "loginButton");
 button.innerText = "Sign in!";
 
-const createAccountContainer = document.createElement("div")
-createAccountContainer.classList.add("createAccount")
-const newHere = document.createElement("span")
-newHere.innerText = "New Here?"
-const createAccount = document.createElement("a")
-createAccount.setAttribute("href", "./registerPage/register.html")
-createAccount.innerText = " Create an Account"
+const createAccountContainer = document.createElement("div");
+createAccountContainer.classList.add("createAccount");
+const newHere = document.createElement("span");
+newHere.innerText = "New Here?";
+const createAccount = document.createElement("a");
+createAccount.setAttribute("href", "./registerPage/register.html");
+createAccount.innerText = " Create an Account";
 
-const catPicBox = document.createElement("div")
-catPicBox.classList.add("catPicBox")
-const catOne = document.createElement("img")
-catOne.setAttribute("src", "https://storables.com/wp-content/uploads/2020/07/18-inch-Cat-Shelf-1024x521.jpg")
-catOne.setAttribute("id", "catOne")
-const catTwo = document.createElement("img")
-catTwo.setAttribute("src", "https://media.istockphoto.com/photos/ginger-cat-picture-id1073474110?k=20&m=1073474110&s=612x612&w=0&h=Yxf5oJ9FK7Al3VsMmsQUEm3eRgzeYdenBWfk3zxip1c=")
-catTwo.setAttribute("id", "catTwo")
+const catPicBox = document.createElement("div");
+catPicBox.classList.add("catPicBox");
+const catOne = document.createElement("img");
+catOne.setAttribute(
+  "src",
+  "https://storables.com/wp-content/uploads/2020/07/18-inch-Cat-Shelf-1024x521.jpg"
+);
+catOne.setAttribute("id", "catOne");
+const catTwo = document.createElement("img");
+catTwo.setAttribute(
+  "src",
+  "https://media.istockphoto.com/photos/ginger-cat-picture-id1073474110?k=20&m=1073474110&s=612x612&w=0&h=Yxf5oJ9FK7Al3VsMmsQUEm3eRgzeYdenBWfk3zxip1c="
+);
+catTwo.setAttribute("id", "catTwo");
 
-loginBox.classList.add("loginBox")
-loginContent.classList.add("loginContent")
-h1.innerText = "Cat Image Generator"
-welcomeMsg.innerText = "Welcome Back!"
-sleepyCat.setAttribute("id", "sleepyCat")
-sleepyCat.setAttribute("src", "https://media.istockphoto.com/photos/sleepy-kitten-picture-id146869137?k=20&m=146869137&s=612x612&w=0&h=6A32QaIlhVX1YxGEtcapbc2mI82gCsNmh4D3OhKuz6Y=")
-userLogin.classList.add("userLogin")
+loginBox.classList.add("loginBox");
+loginContent.classList.add("loginContent");
+h1.innerText = "Cat Image Generator";
+welcomeMsg.innerText = "Welcome Back!";
+sleepyCat.setAttribute("id", "sleepyCat");
+sleepyCat.setAttribute(
+  "src",
+  "https://media.istockphoto.com/photos/sleepy-kitten-picture-id146869137?k=20&m=146869137&s=612x612&w=0&h=6A32QaIlhVX1YxGEtcapbc2mI82gCsNmh4D3OhKuz6Y="
+);
+userLogin.classList.add("userLogin");
 
-mainContainer.appendChild(loginBox)
-loginBox.appendChild(loginContent)
-loginContent.appendChild(h1)
-loginContent.appendChild(welcomeMsg)
-loginContent.appendChild(sleepyCat)
-loginContent.appendChild(userLogin)
+mainContainer.appendChild(loginBox);
+loginBox.appendChild(loginContent);
+loginContent.appendChild(h1);
+loginContent.appendChild(welcomeMsg);
+loginContent.appendChild(sleepyCat);
+loginContent.appendChild(userLogin);
 
-userLogin.appendChild(inputEmailAddress)
-userLogin.appendChild(labelEmailAddress)
-labelEmailAddress.appendChild(emailAddress)
+userLogin.appendChild(inputEmailAddress);
+userLogin.appendChild(labelEmailAddress);
+labelEmailAddress.appendChild(emailAddress);
 
-userLogin.appendChild(inputPassword)
-userLogin.appendChild(labelPassword)
-labelPassword.appendChild(password)
+userLogin.appendChild(inputPassword);
+userLogin.appendChild(labelPassword);
+labelPassword.appendChild(password);
 
-userLogin.appendChild(loginError)
+userLogin.appendChild(loginError);
 
-userLogin.appendChild(href)
-href.appendChild(button)
+userLogin.appendChild(href);
+href.appendChild(button);
 
-loginContent.appendChild(createAccountContainer)
-createAccountContainer.appendChild(newHere)
-createAccountContainer.appendChild(createAccount)
+loginContent.appendChild(createAccountContainer);
+createAccountContainer.appendChild(newHere);
+createAccountContainer.appendChild(createAccount);
 
-mainContainer.appendChild(catPicBox)
-catPicBox.appendChild(catOne)
-catPicBox.appendChild(catTwo)
+mainContainer.appendChild(catPicBox);
+catPicBox.appendChild(catOne);
+catPicBox.appendChild(catTwo);
