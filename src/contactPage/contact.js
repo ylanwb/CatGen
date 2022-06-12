8032de4ce9c31ec5ccf6a20c377f3995ad224162
Chingun <zolboo@Chinguns-iMac.local>
2022-06-12 13:57:55 -0700

    finished stuff

---
+++ src/contactPage/contact.js
@@ -0,0 +1,22 @@
+const userLoggedIn = localStorage.getItem("user");
+const signInButton = document.getElementById("signInBtn");
+const signInImg = document.getElementById("signInImg");
+const href = document.getElementById("signInHref");
+if (userLoggedIn === "true") {
+  console.log("loggedin");
+
+  signInImg.src = "https://cdn-icons-png.flaticon.com/512/126/126467.png"
+  const newText = document.createElement("span")
+  newText.innerText = "Sign Out"
+  signInButton.innerText = ""
+  signInButton.appendChild(signInImg)
+  signInButton.appendChild(newText)
+  
+  href.href = "contact.html";
+} else {
+  console.log("logout");
+  // signInButton.textContent = "Login";
+}
+signInButton.addEventListener("click", () => {
+    localStorage.removeItem("user");
+  });
